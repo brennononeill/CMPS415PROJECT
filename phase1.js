f4fba13f84ffe207e703eb6d5cc48373fe887b1f
brennononeill <89479565+brennononeill@users.noreply.github.com>
2023-04-07 21:29:46 -0500

    Update phase1.js

--- phase1.js
+++ phase1.js
@@ -10,6 +10,25 @@ const app = express();
 const port = 3000;
 var fs = require("fs");
 
+var express = require('express');
+var chalk = require('chalk');
+var app = express();
+var router = express.Router();
+
+router.get('/test', function(req, res) {
+    res.status(200).send('Hello world');
+});
+
+app.use('/api', router);
+
+app.listen(1069, function(err) {
+    if (err) {
+        console.log(chalk.red(err));
+    } else {
+        console.log(chalk.blue('Magic Happens on Port 69'));
+    }
+});
+
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
 
