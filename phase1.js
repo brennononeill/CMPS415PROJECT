82973a58ddf6ad4153ec8e6f60ab509fd2816c9d
brennononeill <89479565+brennononeill@users.noreply.github.com>
2023-04-07 21:24:33 -0500

    Update phase1.js

--- phase1.js
+++ phase1.js
@@ -4,6 +4,57 @@ const bodyParser = require('body-parser');
 // Create Express app
 const app = express();
 
+const express = require('express');
+const bodyParser=require('body-parser');
+const app = express();
+const port = 3000;
+var fs = require("fs");
+
+app.listen(port);
+console.log('Server started at http://localhost:' + port);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// routes will go here
+
+app.get('/', function(req, res) {
+  const myquery = req.query;
+  var outstring = 'Starting... ';
+  res.send(outstring);
+});
+
+
+// Write to a file 
+
+app.get('/wfile', function(req, res) {
+  const myquery = req.query;
+  
+  var outstring = '';
+  for(var key in myquery) { outstring += "--" + key + ">" + myquery[key]; }
+  fs.appendFile("mydata.txt", outstring+'\n', (err) => {
+    if (err)
+      console.log(err);
+    else {
+      console.log("File written successfully\n");
+      console.log("Contents of file now:\n");
+      console.log(fs.readFileSync("mydata.txt", "utf8"));
+    }
+  });
+ 
+  res.send(outstring);
+
+});
+
+
+// Simple cascade
+app.param('name', function(req, res, next, name) {
+  const modified = name.toUpperCase();
+
+  req.name = modified;
+  next();
+});
+
 // Middleware for parsing JSON requests
 app.use(bodyParser.json());
 
