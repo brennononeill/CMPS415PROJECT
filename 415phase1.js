b4ff56d3fb879922945f6e6ca44bdf69c873f9bf
brennononeill <89479565+brennononeill@users.noreply.github.com>
2023-04-13 23:17:45 -0500

    Update 415phase1.js

--- 415phase1.js
+++ 415phase1.js
@@ -1,127 +1,87 @@
 const express = require('express');
-const fs = require('fs').promises;
+const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const FILE_PATH = __dirname + '/data.json';
-
-async function readTickets() {
-  try {
-    const data = await fs.readFile(FILE_PATH);
-    return JSON.parse(data);
-  } catch (err) {
-    console.error(err);
-    return [];
+let tickets = [
+  {
+    "id": 35436,
+    "created_at": "2015-07-20T22:55:29Z",
+    "updated_at": "2016-05-05T10:38:52Z",
+    "type": "incident",
+    "subject": "MFP not working right",
+    "description": "PC Load Letter? What does that even mean???",
+    "priority": "med",
+    "status": "open",
+    "recipient": "support_example@selu.edu",
+    "submitter": "Michael_bolton@selu.edu",
+    "assignee_id": 235323,
+    "follower_ids": [
+        235323,
+        234
+    ],
+    "tags": [
+        "enterprise",
+        "printers"
+    ]
   }
-}
-
-async function writeTickets(tickets) {
-  try {
-    await fs.writeFile(FILE_PATH, JSON.stringify(tickets));
-  } catch (err) {
-    console.error(err);
-  }
-}
-
-// Middleware to parse request bodies
-app.use(express.json());
+];
 
 // Endpoint to get all tickets
-app.get('/rest/list', async (req, res) => {
-  try {
-    const tickets = await readTickets();
-    res.send(tickets);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Something went wrong!');
-  }
+app.get('/rest/list', (req, res) => {
+  res.send(tickets);
 });
 
 // Endpoint to get a single ticket by id
-app.get('/rest/ticket/:id', async (req, res) => {
-  try {
-    const id = parseInt(req.params.id);
-    const tickets = await readTickets();
-    const ticket = tickets.find((t) => t.id === id);
+app.get('/rest/ticket/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const ticket = tickets.find((t) => t.id === id);
 
-    if (!ticket) {
-      res.status(404).send('Ticket not found');
-    } else {
-      res.send(ticket);
-    }
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Something went wrong!');
+  if (!ticket) {
+    res.status(404).send('Ticket not found');
+  } else {
+    res.send(ticket);
   }
 });
 
 // Endpoint to create a new ticket
-app.post('/rest/ticket', async (req, res) => {
-  try {
-    const tickets = await readTickets();
-    const ticket = req.body;
-    ticket.id = Date.now(); // Assign a unique id
-    tickets.push(ticket);
-    console.log(`Created ticket with id ${ticket.id}`);
-
-    await writeTickets(tickets);
-
-    res.send(ticket);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Something went wrong!');
-  }
+app.post('/rest/ticket', express.json(), (req, res) => {
+  const ticket = req.body;
+  ticket.id = Date.now(); // Assign a unique id
+  tickets.push(ticket);
+  console.log(`Created ticket with id ${ticket.id}`);
+  res.send(ticket);
 });
 
-// Endpoint to update an existing ticket by id
-app.put('/rest/ticket/:id', async (req, res) => {
-  try {
-    const id = parseInt(req.params.id);
-    const tickets = await readTickets();
-    const index = tickets.findIndex((t) => t.id === id);
+// Endpoint to update a ticket by id
+app.put('/rest/ticket/:id', express.json(), (req, res) => {
+  const id = parseInt(req.params.id);
+  const index = tickets.findIndex((t) => t.id === id);
 
-    if (index === -1) {
-      res.status(404).send('Ticket not found');
-    } else {
-      tickets[index] = { ...tickets[index], ...req.body };
-
-      await writeTickets(tickets);
-
-      res.send(tickets[index]);
-    }
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Something went wrong!');
+  if (index === -1) {
+    res.status(404).send('Ticket not found');
+  } else {
+    tickets[index] = req.body;
+    tickets[index].id = id;
+    console.log(`Updated ticket with id ${id}`);
+    res.send(tickets[index]);
   }
 });
 
 // Endpoint to delete a ticket by id
-app.delete('/rest/ticket/:id', async (req, res) => {
-  try {
-    const id = parseInt(req.params.id);
-    const tickets = await readTickets();
-    const index = tickets.findIndex((t) => t.id === id);
+app.delete('/rest/ticket/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const index = tickets.findIndex((t) => t.id === id);
 
-    if (index === -1) {
-      res.status(404).send('Ticket not found');
-    } else {
-      const deletedTicket = tickets.splice(index, 1)[0];
-
-      await writeTickets(tickets);
-
-      res.send(deletedTicket);
-    }
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Something went wrong!');
+  if (index === -1) {
+    res.status(404).send('Ticket not found');
+  } else {
+    tickets.splice(index, 1);
+    console.log(`Deleted ticket with id ${id}`);
+    res.sendStatus(204);
   }
 });
-
-// Error handling middleware
-app.use(function(err, req, res, next) {
-  console.error(err.stack)
-
 
 // Start the server
 app.listen(PORT, () => {
