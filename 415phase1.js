222763f4e7fc560d32de620dee2a8421710cd397
brennononeill <89479565+brennononeill@users.noreply.github.com>
2023-04-13 23:57:54 -0500

    Update 415phase1.js

--- 415phase1.js
+++ 415phase1.js
@@ -1,42 +1,61 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const fs = require('fs');
 
-// Create Express app
 const app = express();
-
-// Middleware for parsing JSON requests
-app.use(bodyParser.json());
+const PORT = process.env.PORT || 3000;
 
 // In-memory storage for tickets
-let tickets = [];
+let tickets = [
+  {
+    "id": 35436,
+    "created_at": "2015-07-20T22:55:29Z",
+    "updated_at": "2016-05-05T10:38:52Z",
+    "type": "incident",
+    "subject": "MFP not working right",
+    "description": "PC Load Letter? What does that even mean???",
+    "priority": "med",
+    "status": "open",
+    "recipient": "support_example@selu.edu",
+    "submitter": "Michael_bolton@selu.edu",
+    "assignee_id": 235323,
+    "follower_ids": [
+        235323,
+        234
+    ],
+    "tags": [
+        "enterprise",
+        "printers"
+    ]
+  }
+];
 
 // GET endpoint to get all tickets
 app.get('/rest/list', (req, res) => {
-  res.json(tickets);
+  res.send(tickets);
 });
 
 // GET endpoint to get a single ticket by ID
 app.get('/rest/ticket/:id', (req, res) => {
-  const ticketId = req.params.id;
-  const ticket = tickets.find(ticket => ticket.id === ticketId);
+  const id = parseInt(req.params.id);
+  const ticket = tickets.find((t) => t.id === id);
 
-  if (ticket) {
-    res.json(ticket);
+  if (!ticket) {
+    res.status(404).send('Ticket not found');
   } else {
-    res.status(404).json({ error: 'Ticket not found' });
+    res.send(ticket);
   }
 });
 
 // POST endpoint to create a new ticket
-app.post('/rest/ticket', (req, res) => {
-  const newTicket = req.body;
-  newTicket.id = Date.now().toString(); // Assign a unique ID to the new ticket
-  tickets.push(newTicket);
-  res.json(newTicket);
+app.post('/rest/ticket', express.json(), (req, res) => {
+  const ticket = req.body;
+  ticket.id = Date.now(); // Assign a unique id
+  tickets.push(ticket);
+  console.log(`Created ticket with id ${ticket.id}`);
+  res.send(ticket);
 });
 
 // Start the server
-app.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
-
