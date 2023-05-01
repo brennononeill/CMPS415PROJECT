cf7c26e5a62c39d1e381728f8962c4388400fcca
brennononeill <89479565+brennononeill@users.noreply.github.com>
2023-04-30 22:28:06 -0500

    Update 415phase3.js

--- 415phase3.js
+++ 415phase3.js
@@ -68,6 +68,17 @@ app.get('/createxml', (req, res) => {
   });
 });
 
+app.get('/listbyID', (req, res) => {
+  fs.readFile('./createxml.html', 'utf8', (err, data) => {
+    if (err) {
+      console.error('Failed to read file:', err);
+      res.status(500).send('Failed to read file');
+    } else {
+      res.send(data);
+    }
+  });
+});
+
 app.get('/', function(req, res) {
     res.setHeader('Content-Type', 'text/html');
     fs.readFile('./main.html', 'utf8', (err, contents) => {
