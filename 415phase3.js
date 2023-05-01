2626d0d3bd064970b5a053950328d459ba7096a7
brennononeill <89479565+brennononeill@users.noreply.github.com>
2023-05-01 01:08:09 -0500

    Update 415phase3.js

--- 415phase3.js
+++ 415phase3.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
+const { MongoClient } = require("mongodb");
 const fs = require('fs');
 const app = express();
 const xml2js = require('xml2js');
